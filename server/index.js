53bba418f9799d94cd7cac5fd5d6103a4482e169
shubhanshurav <shubhanshurao7037@gmail.com>
2024-09-10 21:23:07 +0530

    update the code

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "*", 
+    origin: "*" || "https://shubhanshu-foodcircles.vercel.app/",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   })
