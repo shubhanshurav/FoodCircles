04928d75787a47bfe2a6b5a3ae64279e210d4a7d
shubhanshurav <shubhanshurao7037@gmail.com>
2024-09-10 21:58:32 +0530

    update the code

--- src/constants.js
+++ src/constants.js
@@ -4,5 +4,5 @@ export const LOGO = "./assets/Logo.jpeg";
 // export const MENU_API = "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
 
 export const shimmer_card_unit = 20;
-// shimmer Menu card unit
+// shimmer Menu card unit  
 export const shimmer_menu_card_unit = 4;
