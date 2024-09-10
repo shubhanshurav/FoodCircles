4f5027627a521602a525a88b2ab52c66b9f668de
shubhanshurav <shubhanshurao7037@gmail.com>
2024-09-10 21:50:11 +0530

    Merge branch 'master' of https://github.com/shubhanshurav/FoodCircles

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(
+app.use( 
   cors({
     origin: "*" || "https://shubhanshu-foodcircles.vercel.app/",
     methods: ["GET", "POST"],
